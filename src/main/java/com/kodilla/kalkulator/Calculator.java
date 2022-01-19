efe0983138f63177bd92947900c5cadca74b3043
Krystian Krawiec <kkicur1@tlen.pl>
2022-01-19 21:31:06 +0100

    First Commit

--- src/main/java/com/kodilla/kalkulator/Calculator.java
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -1,4 +1,20 @@
 package com.kodilla.kalkulator;
 
 public class Calculator {
+
+    public double add(double a, double b) {
+        return a+b;
+    }
+
+    public double substraction(double a, double b) {
+        return a-b;
+    }
+
+    public static void main (String args[]) {
+        Calculator calculator= new Calculator();
+        double resultPlus=calculator.add(4,2);
+        double resultMinus=calculator.substraction(3,1);
+        System.out.println("Sum result is: "+ resultPlus);
+        System.out.println("Substraction result is: "+ resultMinus);
+    }
 }
